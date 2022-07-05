1e209256fbc7ed9171207dcee3a6d20d172eb5a1
nico <varela.nicoo@gmail.com>
2022-07-05 11:47:22 -0300

    act

--- db/config.js
+++ db/config.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql")
 const util = require("util")
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.db_host,
     database: process.env.db_name,
     user: process.env.db_user,
     password: process.env.db_pass
 });
 
-connection.connect((err) => {
-    err ? console.warn("No conectado", { "Error": err.message }) : console.dir("Conexión establecida...")
+pool.getConnection((err) => {
+    err ? console.log(`Error encontrado:${err}`):console.log("Conexión establecida...")
 })
 
-connection.query = util.promisify(connection.query)
+pool.query=util.promisify(pool.query)
 
-module.exports = connection
+module.exports = pool
